d9bcfe45cd14a9ca33aea882b5f16fcbd6a77331
Karp <heniulu10@gmail.com>
2023-03-03 09:34:37 +0100

    fix: change print

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    print('asad');
+    print('asaad');
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
